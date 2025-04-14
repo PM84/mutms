fe795e3e052bd07a3f463f9cdf080b91cfdab2e5
Petr Skoda <petr@mutms.org>
2025-04-14 11:32:23 +0200

    Squashed 'blocks/mucertify_my/' content from commit ae52f4bae42
    git-subtree-dir: blocks/mucertify_my
    git-subtree-split: ae52f4bae425f7c643dc0895c3210fb3f3789fdd

---
+++ version.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Certifications for Moodle™.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+/**
+ * My certifications overview block.
+ *
+ * @package     block_mucertify_my
+ * @copyright   2023 Open LMS (https://www.openlms.net/)
+ * @copyright   2025 Petr Skoda
+ * @author      Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/** @var stdClass $plugin */
+$plugin->component = 'block_mucertify_my';
+$plugin->version   = 2025041400;
+$plugin->requires  = 2024091700.00;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->supported = [405, 405];
+$plugin->release   = 'mu-4.5.4-01';
+
+$plugin->dependencies = [
+    'tool_mucertify' => 2025041400,
+];
