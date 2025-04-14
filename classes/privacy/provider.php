6038a4980fa19da83fa4601e795c7a79706c962e
Petr Skoda <petr@mutms.org>
2025-04-14 11:32:03 +0200

    Squashed 'admin/tool/mulib/' content from commit 58f0ef09723
    git-subtree-dir: admin/tool/mulib
    git-subtree-split: 58f0ef09723f21f9abaa07ad309c1fe0132a7cbb

---
+++ classes/privacy/provider.php
@@ -0,0 +1,112 @@
+<?php
+// This file is part of Additional tools library for Moodle™.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+namespace tool_mulib\privacy;
+
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\userlist;
+
+/**
+ * Open LMS support class privacy support.
+ *
+ * @package     tool_mulib
+ * @copyright   2022 Open LMS (https://www.openlms.net/)
+ * @copyright   2025 Petr Skoda
+ * @author      Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\plugin\provider,
+    \core_privacy\local\request\core_userlist_provider {
+
+    /**
+     * Returns meta data about this system.
+     *
+     * @param collection $collection The initialised collection to add items to.
+     * @return collection A listing of user data stored through this system.
+     */
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_database_table(
+            'tool_mulib_notification_user',
+            [
+                'userid' => 'privacy:metadata:userid',
+            ],
+            'privacy:metadata:tool_mulib_notification_user:tableexplanation'
+        );
+        return $collection;
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid The user to search.
+     * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        return new contextlist();
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+        // This will be tricky.
+    }
+
+    /**
+     * Delete all data for all users in the specified context.
+     *
+     * @param \context $context The specific context to delete data for.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context) {
+        // This will be tricky.
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function delete_data_for_user(approved_contextlist $contextlist) {
+        // This will be tricky.
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        // This will be tricky.
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+        // This will be tricky.
+    }
+}
---
+++ notification/update.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Additional tools library for Moodle™.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+/**
+ * Update existing notification.
+ *
+ * @package     tool_mulib
+ * @copyright   2022 Open LMS
+ * @copyright   2025 Petr Skoda
+ * @author      Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use tool_mulib\local\notification\util;
+
+/** @var moodle_database $DB */
+/** @var moodle_page $PAGE */
+/** @var core_renderer $OUTPUT */
+/** @var stdClass $CFG */
+
+// phpcs:ignoreFile moodle.Files.MoodleInternal.MoodleInternalGlobalState
+if (!empty($_SERVER['HTTP_X_MULIB_DIALOG_FORM_REQUEST'])) {
+    define('AJAX_SCRIPT', true);
+}
+require('../../../../config.php');
+
+$id = required_param('id', PARAM_INT);
+
+require_login();
+
+$notification = $DB->get_record('tool_mulib_notification', ['id' => $id], '*', MUST_EXIST);
+
+/** @var class-string<\tool_mulib\local\notification\manager> $manager */
+$manager = \tool_mulib\local\notification\util::get_manager_classname($notification->component);
+if (!$manager) {
+    throw new invalid_parameter_exception('Invalid notification component');
+}
+
+$returnurl = $manager::get_instance_management_url($notification->instanceid);
+if (!$manager::can_manage($notification->instanceid)) {
+    redirect($returnurl);
+}
+
+$context = $manager::get_instance_context($notification->instanceid);
+
+$PAGE->set_context($context);
+$PAGE->set_url('/admin/tool/mulib/notification/update.php', ['id' => $notification->id]);
+$PAGE->set_pagelayout('admin');
+$PAGE->set_heading(get_string('notification_update', 'tool_mulib'));
+$PAGE->set_title(get_string('notification_update', 'tool_mulib'));
+
+$form = new \tool_mulib\local\form\notification_update(null, ['notification' => $notification, 'manager' => $manager]);
+if ($form->is_cancelled()) {
+    redirect($returnurl);
+} else if ($data = $form->get_data()) {
+    $notification = util::notification_update((array)$data);
+    $returnurl = new moodle_url('/admin/tool/mulib/notification/view.php', ['id' => $notification->id]);
+    $form->redirect_submitted($returnurl);
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('notification_update', 'tool_mulib'));
+echo $form->render();
+echo $OUTPUT->footer();
