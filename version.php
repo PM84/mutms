00f3d8d80f9076a13558ca039702cce355e99869
Petr Skoda <petr@mutms.org>
2025-05-01 06:55:29 +0200

    Squashed 'blocks/muprog_my/' content from commit 7a168740c96
    git-subtree-dir: blocks/muprog_my
    git-subtree-split: 7a168740c963d855e04a330a4c4b37f355f0f8f1

---
+++ version.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Programs for Moodle™.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+/**
+ * My programs overview block.
+ *
+ * @package     block_muprog_my
+ * @copyright   2022 Open LMS (https://www.openlms.net/)
+ * @copyright   2025 Petr Skoda
+ * @author      Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/** @var stdClass $plugin */
+$plugin->component = 'block_muprog_my';
+$plugin->version   = 2025042900;
+$plugin->requires  = 2024091700.00;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->supported = [405, 405];
+$plugin->release   = 'mu-4.5.4-09';
+
+$plugin->dependencies = [
+    'tool_muprog' => 2025042900,
+];
